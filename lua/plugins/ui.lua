3bd818facaf38d518e89d66545203679b0ca575a
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-08-06 14:11:01 +0800

    style: lint

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -371,11 +371,11 @@ return {
 		"folke/trouble.nvim",
 	},
 	{
-        "folke/todo-comments.nvim",
+		"folke/todo-comments.nvim",
 		event = "BufRead",
 		dependencies = { "nvim-lua/plenary.nvim", "folke/trouble.nvim" },
 		config = function()
-            require("todo-comments").setup({})
+			require("todo-comments").setup({})
 			-- Setup Trouble
 			vim.cmd("TodoTrouble")
 			vim.cmd("TroubleClose")
