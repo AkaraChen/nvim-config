e7fff8dc107fb5bb923ae3b4c69c53a3926de39c
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-08-06 13:45:30 +0800

    style: lint

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -362,8 +362,8 @@ return {
 		"goolord/alpha-nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function ()
+		config = function()
 			require("alpha").setup(require("alpha.themes.dashboard").config)
-		end
+		end,
 	},
 }
