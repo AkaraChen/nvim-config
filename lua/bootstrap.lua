5b3095d9494cb6b68386938a1e5a3ecc2cb69b1d
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-08-06 10:11:19 +0800

    refactor: move plugin config

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -1,19 +1,2 @@
--- Open File Explorer
-vim.cmd("Neotree")
-
--- Status Bar
-require("plugin-config.status")
-
--- Color Schema
-vim.cmd("colorscheme onedark")
-
--- LSP installer
-require("mason").setup()
-
--- Auto complete
-require("plugin-config.cmp")
-
 -- Setup LSP
 require("lsp.main")
-
-require("bufferline").setup({})
--- lua/plugins/complete.lua
+++ lua/plugins/complete.lua
@@ -1,5 +1,17 @@
 return {
 	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"onsails/lspkind.nvim",
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		event = { "InsertEnter", "CmdlineEnter" },
 		dependencies = {
@@ -8,8 +20,50 @@ return {
 			{ "hrsh7th/cmp-cmdline" },
 			{ "hrsh7th/cmp-path" },
 		},
+		config = function()
+			-- Setup auto complete
+			local status, cmp = pcall(require, "cmp")
+			if not status then
+				return
+			end
+			local lspkind = require("lspkind")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+				formatting = {
+					format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
+				},
+			})
+
+			vim.cmd([[
+  set completeopt=menuone,noinsert,noselect
+  highlight! default link CmpItemKind CmpItemMenuDefault
+]])
+		end,
 	},
 	{
-		"github/copilot.vim"
+		"github/copilot.vim",
+	},
+	{
+		"L3MON4D3/LuaSnip",
 	},
 }
