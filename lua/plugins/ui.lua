245fc8a19f4d79b482378d63ee8bffb2572d40cb
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-09-10 09:14:23 +0800

    fix: buf close (fuck nvim)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -201,19 +201,17 @@ return {
 		'jubnzv/virtual-types.nvim',
 	},
 	{
-		'akinsho/bufferline.nvim',
-		config = function()
-			require('bufferline').setup {
-				options = {
-					offsets = {
-						{
-							filetype = 'neo-tree',
-							text = '󰈔 File',
-							highlight = 'Directory',
-						},
-					},
-				},
-			}
-		end,
+		'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function() vim.g.barbar_auto_setup = false end,
+		opts = {
+			sidebar_filetypes = {
+				['neo-tree'] = { event = 'BufWipeout', text = '󰈔 File' },
+            },
+			animation = false,
+		},
 	},
 }
