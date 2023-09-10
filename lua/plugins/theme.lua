47de1a64f656026f1ab07bb6db4c40cc3e5311aa
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-09-10 17:26:15 +0800

    chore: theme switcher

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,28 @@
 return {
 	{
+		'zaldih/themery.nvim',
+		config = function()
+			local config_path = '~/.config/nvim/lua/config/theme.lua'
+			require('themery').setup {
+				themes = {
+					'onedark',
+					'catppuccin-latte',
+					'catppuccin-frappe',
+					'catppuccin-macchiato',
+					'catppuccin-mocha',
+				},
+				themeConfigFile = config_path,
+				livePreview = true,
+			}
+			-- if config_path is exist, require it:
+			if vim.fn.filereadable(vim.fn.expand(config_path)) == 1 then
+				require 'config.theme'
+			else
+				vim.cmd 'colorscheme catppuccin-frappe'
+			end
+		end,
+	},
+	{
 		'olimorris/onedarkpro.nvim',
 		priority = 1000,
 		config = function()
@@ -10,7 +33,7 @@ return {
 		'catppuccin/nvim',
 		name = 'catppuccin',
 		config = function()
-			vim.cmd 'colorscheme catppuccin-latte'
+			-- vim.cmd 'colorscheme catppuccin-latte'
 		end,
 		priority = 1000,
 	},
